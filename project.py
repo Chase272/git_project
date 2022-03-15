4507715d587ccd03462b749c119af1d8f49068bf
Chaitanya <Chaitanyarohankar27@gmail.com>
2022-03-15 23:17:30 +0530

    Chnaege

--- project.py
+++ project.py
@@ -3,8 +3,14 @@ def add():
     a = a + 1
     return a 
 
+def subtract():
+    a = 10
+    a = a  - 1
+    return a
+
 new_add = add()
 
 print(new_add)
+print(subtract())
 print("Hello Friend")
 print("Hello World")
