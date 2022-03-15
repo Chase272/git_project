065d49990fb591cf0bcc7eaae7dfc428a4ea7d23
Chaitanya <Chaitanyarohankar27@gmail.com>
2022-03-15 23:34:39 +0530

    Multiply Feature

--- project.py
+++ project.py
@@ -1,9 +1,18 @@
+from re import A
+
+
 def add():
     a = 10
     a = a + 1
     return a 
-
 new_add = add()
+
+
+def multiply():
+    a = 30 
+    a = a*a
+    return a 
+
 
 print(new_add)
 print("Hello Friend")
