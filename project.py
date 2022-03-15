0c0ae4b913fd8d557ef0eee38ca4889b19f7bf26
Chaitanya <Chaitanyarohankar27@gmail.com>
2022-03-15 23:15:43 +0530

    Subtract function add

--- project.py
+++ project.py
@@ -3,14 +3,8 @@ def add():
     a = a + 1
     return a 
 
-def subtract():
-    a = 10
-    a = a  - 1
-    return a
-
 new_add = add()
 
 print(new_add)
-print(subtract())
 print("Hello Friend")
 print("Hello World")
